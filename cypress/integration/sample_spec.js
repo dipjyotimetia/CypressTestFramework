5c6ad538bf7218a4a70d92ef9adf38a8e083f2a0
Dipjyoti Metia <dipjyotimetia@gmail.com>
2019-01-16 20:50:28 +1100

    test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -4,7 +4,7 @@ describe('Testing the hero list page', function () {
 
     it('should contain the word heroes', function () {
         cy.visit('http://automationpractice.com/index.php');
-        cy.contains('login').click();
+        cy.get('.login').click();
         cy.screenshot();
     })
 
