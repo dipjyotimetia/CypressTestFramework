5f60619176f3ce94ca6d30044b733a98ccba9495
Dipjyoti Metia <dipjyotimetia@gmail.com>
2019-02-13 23:29:01 +1100

    update

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -6,6 +6,21 @@ describe('Testing the hero list page', function () {
         cy.visit('http://automationpractice.com/index.php');
         cy.get('.login').click();
         cy.screenshot();
-    })
+    });
 
+    it('', () => {
+        cy.visit('http://automationpractice.com/index.php')
+            .get('.login')
+            .click()
+            .screenshot();
+    });
+
+    // Cypress.Commands.add("state", (...states) => {
+    //     cy.request({
+    //         url: `${Cypress.env('BASE_API')}/integration/state`,
+    //         method: 'POST',
+    //         headers: { 'content-type': 'application/json' },
+    //         body: JSON.stringify({ states: states })
+    //     });
+    // });
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 .\node_modules\.bin\cypress open
 
 [![CircleCI](https://circleci.com/gh/dipjyotimetia/CypressTest/tree/master.svg?style=svg)](https://circleci.com/gh/dipjyotimetia/CypressTest/tree/master)
+
+
+https://hackernoon.com/cypress-io-docker-the-ultimate-e2e-stack-a20ee25654b1
+
+https://medium.freecodecamp.org/how-to-test-your-frontend-with-the-cypress-io-framework-f048070f4330
+
