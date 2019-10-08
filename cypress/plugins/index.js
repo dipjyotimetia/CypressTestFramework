84fd64736ac69d7de482cd0c57d62dfee3d61cc3
Dipjyoti Metia <dipjyotimetia@gmail.com>
2019-10-08 23:48:47 +1100

    updated framework

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,17 +1,16 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const ntlmAuth = require('cypress-ntlm-auth/dist/plugin');
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
+  on('before:browser:launch', (browser = {}, args) => {
+
+    // config = ntlmAuth.initNtlmAuth(config);
+
+    if (browser.name === 'chrome') {
+      args.push('--remote-debugging-port=9222')
+
+      return args
+    }
+
+  })
 }
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,5 @@
 require('cypress-xpath');
 import './commands';
 require('cypress-dark');
+// import 'cypress-ntlm-auth/dist/commands';
+require('cypress-plugin-retries')
