bd46a159679af8f1e33980428ec197e9c857391b
Dipjyoti Metia <dipjyoti.metia@beteasy.com.au>
2019-10-24 14:01:03 +1100

    updated

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,5 +1,5 @@
 require('cypress-xpath');
 import './commands';
 require('cypress-dark');
-// import 'cypress-ntlm-auth/dist/commands';
-require('cypress-plugin-retries')
+import 'cypress-ntlm-auth/dist/commands';
+require('cypress-plugin-retries');
