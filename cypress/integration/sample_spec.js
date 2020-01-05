4f44b2bb300b0be9111c29a7e9c934e8db2ad68a
Dipjyoti Metia <dipjyotimetia@gmail.com>
2020-01-05 18:58:33 +1100

    final update

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,12 +1,13 @@
 /// <reference types="Cypress" />
 
 import { loginPage } from "../support/pageObjects/login_page";
+import { BASE_URL, USERNAME, PASSWORD } from "../config/config";
 
 describe('Testing BetEasy Web', function () {
 
     it('BetEasy Login', () => {
-        loginPage.navigateToUrl('https://beteasy.com.au');
-        loginPage.login('', '');
+        loginPage.navigateToUrl(BASE_URL);
+        loginPage.login(USERNAME, PASSWORD);
     });
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,10 +2,10 @@ import 'cypress-file-upload';
 import '@testing-library/cypress/add-commands'
 
 Cypress.Commands.add("login", (...states) => {
-        cy.request({
-            url: `${Cypress.env('BASE_API')}/integration/state`,
-            method: 'POST',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify({ states: states })
-        });
+    cy.request({
+        url: `${Cypress.env('BASE_API')}/integration/state`,
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ states: states })
     });
+});
