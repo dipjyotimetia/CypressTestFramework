2158093f9a12f73205a2a9bf7c27a2499b4dc0b5
Dipjyoti Metia <dipjyotimetia@gmail.com>
2020-01-17 15:58:06 +1100

    Update index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,6 +2,15 @@ require('cypress-xpath');
 import './commands';
 import 'cypress-ntlm-auth/dist/commands';
 require('cypress-plugin-retries');
+const addContext = require('mochawesome/addContext');
+
+Cypress.on('test:after:run', (test, runnable) => {
+    if(test.state == 'failed') {
+        addContext({ test }, `../cypress/screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`)
+    }
+    //If video is required
+    //addContext({ test },  `../cypress/videos/${Cypress.spec.name}.mp4`);
+});
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
@@ -11,3 +20,4 @@ Cypress.on('fail', (error, runnable) => {
     debugger
     throw error;
   })
+
