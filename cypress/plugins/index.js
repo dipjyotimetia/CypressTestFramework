3809d9da2bc06d46d9883dc7058e8b90afaed377
Dipjyoti Metia <dipjyotimetia@gmail.com>
2020-01-17 16:00:41 +1100

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,10 @@
 const fs = require('fs-extra');
 const path = require('path');
+const chalk = require('chalk');
+const timeStamp = require('date-fns');
+const { install, ensureRequiredBrowserFlags } = require('@neuralegion/cypress-har-generator');
+
+let logTime = timeStamp.format(new Date(), 'yyyy-MM-dd hh:mm:ss');
 
 const getConfigurationByFile = (file) => {
   const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
@@ -7,13 +12,15 @@ const getConfigurationByFile = (file) => {
 }
 
 module.exports = (on, config) => {
+  install(on,config);
+  // require('cypress-plugin-retries/lib/plugin')(on)
 
   on('before:browser:launch', (browser = {}, args) => {
-
+    ensureRequiredBrowserFlags(browser,args);
     if (browser.name === 'chrome') {
       args.push('--start-fullscreen')
-      args.push('--remote-debugging-port=9222')
-
+      args.push('--no-sandbox')
+      args.push('--disable-gpu')
       return args;
     }
 
@@ -23,6 +30,21 @@ module.exports = (on, config) => {
       return args;
     }
   });
+
+  on('task', {
+    info(message) {
+      console.log(chalk.yellow(`    INFO - ${logTime}; ${message}`))
+      return null
+    }
+  });
+
+  on('task', {
+    fail(message) {
+      console.log(chalk.bgRed(`    ERROR - ${logTime}; ${message}`))
+      return null
+    }
+  });
+
   const file = config.env.configFile || 'test'
 
   return getConfigurationByFile(file);
